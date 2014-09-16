d4ceb91a24e75f058dad903b1f97ccca84864e14
xuanthanh18 <xuanthanh18@gmail.com>
2014-09-16 09:08:54 -0700

    add reset functionalities

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'soundbox.com'
+  host = 'https://ancient-depths-9071.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
