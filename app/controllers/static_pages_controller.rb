f6e91d2d0456b9627fa6ab53b6ea1830213b5248
xuanthanh18 <xuanthanh18@gmail.com>
2014-09-17 00:29:57 -0700

    fix some typos

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      @micropost = current_user.microposts.build if logged_in?git
+      @micropost = current_user.microposts.build if logged_in?
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
---
+++ awsupdate.sh
@@ -0,0 +1,3 @@
+heroku config:set S3_ACCESS_KEY=<access key>
+heroku config:set S3_SECRET_KEY=<secret key>
+heroku config:set S3_BUCKET=<bucket name>
