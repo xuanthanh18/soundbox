0ee9bfaece5e3e528840b0fbab4c6a20971a3c6d
xuanthanh18 <xuanthanh18@gmail.com>
2014-09-18 10:26:06 -0700

    done with pictures

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -24,9 +24,6 @@ class PictureUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -50,9 +47,9 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+     %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
